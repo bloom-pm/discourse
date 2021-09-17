044f3b204b6de63115fea78bd8ec99176c15b07a
Omar Salloum <omar@bloom.pm>
2021-09-17 17:54:22 +0300

    Various fixes

--- docker/discourse_run.sh
+++ docker/discourse_run.sh
@@ -24,7 +24,11 @@ touch /var/www/discourse/tmp/pids/unicorn.pid
 chmod 0777 /var/www/discourse/tmp/pids/unicorn.pid
 touch /var/www/discourse/log/unicorn.stderr.log
 chmod 0777 /var/www/discourse/log/unicorn.stderr.log
+rm -Rf /var/www/discourse/log/production.log
+touch  /var/www/discourse/log/production.log
+chmod 0664  /var/www/discourse/log/production.log
 
+chmod -R 0777 tmp/ember-rails
 runsvdir /etc/service &
 
 LD_PRELOAD=$RUBY_ALLOCATOR HOME=/home/discourse USER=discourse UNICORN_BIND_ALL=1 UNICORN_PORT=3000 exec thpoff chpst -u discourse:www-data -U discourse:www-data bundle exec config/unicorn_launcher -E production -c config/unicorn.conf.rb
