4de07fb4e6981f8fc66c55045db9d736a4698039
Mujtaba Al-Tameemi <mujtaba.altameemi@gmail.com>
2022-04-26 15:20:56 +0300

    feat(discourse_install): handle bundler warnings

--- docker/discourse_install.sh
+++ docker/discourse_install.sh
@@ -3,11 +3,12 @@
 set -x
 set -e
 bundle config set --local deployment 'true'
+bundle config set --local --without test
 bundle config mirror.https://rubygems.org http://localhost:9292
 bundle config mirror.https://github.com http://localhost:9292
 chown -R discourse:discourse /var/www/discourse
 cd /var/www/discourse
-sudo -u discourse bundle install --deployment --jobs 4 --without test
+sudo -u discourse bundle install --jobs 4
 sudo -u discourse yarn install --production
 sudo -u discourse yarn cache clean
 bundle exec rake maxminddb:get
